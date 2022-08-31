a957d340b16609c9b9b60a84c2f89c84266e63c3
Aldwin <aldwin.pag@gmail.com>
2022-08-31 17:49:19 -0400

    Add hover effect to buttons

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/* Tip calculator */
+
+/* DOM Elements */
+// Buttons
+const eighteenTipPercent = document.getElementById()
+
+/* Variables */
+
+/* Functions */
+function calculateTotal(tipPercent, subtotal) {
+
+}
