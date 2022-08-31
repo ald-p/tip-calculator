0f2c1576807f5ce2ea68953ea4b0be5ae9aff243
Aldwin <aldwin.pag@gmail.com>
2022-08-31 18:18:45 -0400

    Give tip percent buttons functionality to find tip percentage and subtotal values

--- js/index.js
+++ js/index.js
@@ -2,11 +2,31 @@
 
 /* DOM Elements */
 // Buttons
-const eighteenTipPercent = document.getElementById()
+const tipPercentSection = document.getElementById('tip-percent-section')
+const customTipBtn = document.getElementById('custom-tip-btn');
+const subtotalValEl = document.getElementById('subtotal-val');
 
 /* Variables */
 
+/* Event listeners */
+tipPercentSection.addEventListener('click', findTotal, false);
+
 /* Functions */
+function findTotal(e) {
+
+  // Find tip percent
+  console.log(e.target.textContent);
+  const btnPercent = e.target.textContent;
+  const tipPercent = parseInt(btnPercent, 10) / 100;
+
+  // Find subtotal value
+  const subtotalVal = subtotalValEl.value;
+  console.log(subtotalVal);
+
+  calculateTotal(tipPercent, subtotalVal);
+}
+
 function calculateTotal(tipPercent, subtotal) {
 
 }
+
