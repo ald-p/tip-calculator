e5c1edf9914d4007356a7b028737d5e946daedb4
Aldwin <aldwin.pag@gmail.com>
2022-08-31 23:01:23 -0400

    Add render function to show tip and total amounts

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@
 const tipPercentSection = document.getElementById('tip-percent-section')
 const customTipBtn = document.getElementById('custom-tip-btn');
 const subtotalValEl = document.getElementById('subtotal-val');
+const tipAmtEl = document.getElementById('tip-amt');
+const totalAmtEl = document.getElementById('total-amt');
 
 /* Variables */
 
@@ -13,17 +15,18 @@ tipPercentSection.addEventListener('click', findTotal, false);
 
 /* Functions */
 function findTotal(e) {
-
   // Find tip percent
-  console.log(e.target.textContent);
   const btnPercent = e.target.textContent;
   const tipPercent = parseInt(btnPercent, 10) / 100;
 
   // Find subtotal value
   const subtotalVal = Number(subtotalValEl.value);
-  console.log(typeof subtotalVal);
 
+  // Calculate tip and total amounts
   const totals = calculateTotal(tipPercent, subtotalVal);
+
+  // Render
+  renderAmounts(totals[0], totals[1]);
 }
 
 function calculateTotal(tipPercent, subtotal) {
@@ -31,5 +34,10 @@ function calculateTotal(tipPercent, subtotal) {
   const totalAmt = subtotal + tipAmt;
 
   return [tipAmt.toFixed(2), totalAmt.toFixed(2)];
+}
+
+function renderAmounts(tipAmt, totalAmt) {
+  tipAmtEl.textContent = `$${tipAmt}`;
+  totalAmtEl.textContent = `$${totalAmt}`;
 }
 
