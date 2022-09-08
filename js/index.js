1d8e0186c6e7070286d8cf88163b3a7bbf4eac59
Aldwin <aldwin.pag@gmail.com>
2022-09-08 17:37:00 -0400

    Hold button styles when clicked and reset other button styles

--- js/index.js
+++ js/index.js
@@ -2,34 +2,47 @@
 
 /* DOM Elements */
 // Buttons
-const tipPercentSection = document.getElementById('tip-percent-section')
-const customTipBtn = document.getElementById('custom-tip-btn');
+const tipPercentSection = document.getElementById('tip-btns')
 const subtotalValEl = document.getElementById('subtotal-val');
 const tipAmtEl = document.getElementById('tip-amt');
 const totalAmtEl = document.getElementById('total-amt');
 const customTipSection = document.getElementById('custom-tip-section');
+const customTipBtn = document.getElementById('custom-tip-btn')
 
 /* Variables */
 
 /* Event listeners */
-tipPercentSection.addEventListener('click', findTotal, false);
-customTipBtn.addEventListener('click', showCustomTipSection, false);
+tipPercentSection.addEventListener('click', tipPercentButtonClicked, false);
 
 /* Functions */
-function findTotal(e) {
+function tipPercentButtonClicked(e) {
+  if (e.target.classList.contains('btn')) {
+    toggleButtonStyles(e.target);
 
-  // Find tip percent
-  const btnPercent = e.target.textContent;
-  const tipPercent = parseInt(btnPercent, 10) / 100;
+    if (e.target.id === 'custom-tip-btn') {
+      showCustomTipSection();
+      return;
+    }
 
-  // Find subtotal value
-  const subtotalVal = Number(subtotalValEl.value);
+    // Used in the case where custom tip section is shown and other percent buttons are clicked. When clicking the other percent buttons, custom tip section must also dissappear
+    if (customTipSection.classList.contains('section-show')) {
+      showCustomTipSection();
+    }
 
-  // Calculate tip and total amounts
-  const totals = calculateTotal(tipPercent, subtotalVal);
+    // Find tip percent
+    const btnPercent = e.target.textContent;
+    const tipPercent = parseInt(btnPercent, 10) / 100;
 
-  // Render
-  renderAmounts(totals[0], totals[1]);
+    // Find subtotal value
+    const subtotalVal = Number(subtotalValEl.value);
+
+    // Calculate tip and total amounts
+    const totals = calculateTotal(tipPercent, subtotalVal);
+
+    // Render
+    renderAmounts(totals[0], totals[1]);
+  }
+  
 }
 
 function calculateTotal(tipPercent, subtotal) {
@@ -48,10 +61,16 @@ function showCustomTipSection() {
   if (customTipSection.classList.contains('section-show')) {
     customTipSection.classList.remove('section-show');
     customTipSection.classList.add('section-no-show');
-    console.log(customTipSection)
+    customTipBtn.classList.remove('btn-clicked');
   } else {
     customTipSection.classList.remove('section-no-show');
     customTipSection.classList.add('section-show');
-    console.log(customTipSection)
+    customTipBtn.classList.add('btn-clicked');
   }
+}
+
+function toggleButtonStyles(targetEl) {
+  const buttons = document.querySelectorAll('.tip-btn');
+  buttons.forEach( btn => btn.classList.remove('btn-clicked'));
+  targetEl.classList.add('btn-clicked');
 }
